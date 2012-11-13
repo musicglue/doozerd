b31562d1bb12f959589fefbb1444f205dc4936e0
John <john@musicglue.com>
2012-11-13 18:09:23 +0000

    dereference moved proto call

--- consensus/m.pb.go
+++ consensus/m.pb.go
@@ -7,7 +7,7 @@ import proto "code.google.com/p/goprotobuf/proto"
 import "math"
 
 // Reference proto and math imports to suppress error if they are not otherwise used.
-var _ = proto.GetString
+var _ = proto.String
 var _ = math.Inf
 
 type msg_Cmd int32
